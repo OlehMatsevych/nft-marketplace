589c31cd3c77d76c410dc1e92cf4c08fd782e317
OlehMatsevych <oleg.macevich.pz.2019.nulp.com@gmail.com>
2023-01-17 22:23:48 +0200

    fix: configs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,27 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-waffle";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+//  mumbai: {
+//    url: "https://rpc-mumbai.maticvigil.com",
+//    accounts: [process.env.privateKey]
+//  }
 };
 
 export default config;
